366c9e9f2ca62cc54bb2c63144e5a4ae42cf767a
Arthur Fonseca <arthurbfonseca27@gmail.com>
2026-04-09 19:14:26 -0300

    pra não perder as coisa ao menos

--- test_triangle.py
+++ test_triangle.py
@@ -14,6 +14,23 @@ def test_isoceles():
     t = Triangle(10, 10, 12)
     assert t.type == TriangleType.ISOSCELES
 
+def test_invalid():
+    t = Triangle(1, 1, 5)
+    assert t.type == TriangleType.INVALID
+
+def test_negative():
+    t = Triangle(-1, 2, 3)
+    assert t.type == TriangleType.INVALID
+
+def test_all_sides_zero():
+    t = Triangle(0, 0, 0)
+    assert t.type == TriangleType.INVALID
+
+def test_float_numbers():
+    t = Triangle(1, 0.99999999999999999999999999999999999, 0.99999999999999999999999999999999999)  
+    assert t.type == TriangleType.EQUILATERAL
+
+
 def test_string():
     t1 = "a"
     t2 = "b"
