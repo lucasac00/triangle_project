f881258527806fdd6b82eb88a7037a7891b7a691
Arthur Fonseca <arthurbfonseca27@gmail.com>
2026-04-09 19:20:57 -0300

    pra não perder as coisa ao menos

--- test_triangle.py
+++ test_triangle.py
@@ -3,31 +3,55 @@ from triangle import Triangle, TriangleType
 
 
 def test_equilateral():
-    t = Triangle(7, 7, 7)
+    t1 = 7
+    t2 = 7
+    t3 = 7
+
+    t = Triangle(t1, t2, t3)
     assert t.type == TriangleType.EQUILATERAL
 
 def test_scalene():
-    t = Triangle(3, 4, 5)
+    t1 = 3
+    t2 = 4
+    t3 = 5
+
+    t = Triangle(t1, t2, t3)
     assert t.type == TriangleType.SCALENE
 
 def test_isoceles():
-    t = Triangle(10, 10, 12)
+    t1 = 10
+    t2 = 10
+    t3 = 12
+
+    t = Triangle(t1, t2, t3)
     assert t.type == TriangleType.ISOSCELES
 
 def test_invalid():
-    t = Triangle(1, 1, 5)
+    t1 = 1
+    t2 = 1
+    t3 = 5  
+
+    t = Triangle(t1, t2, t3)
     assert t.type == TriangleType.INVALID
 
 def test_negative():
-    t = Triangle(-1, 2, 3)
+    t1 = -1
+    t2 = 2
+    t3 = 3
+
+    t = Triangle(t1, t2, t3)
     assert t.type == TriangleType.INVALID
 
 def test_all_sides_zero():
-    t = Triangle(0, 0, 0)
+    t1 = 0
+    t2 = 0
+    t3 = 0
+
+    t = Triangle(t1, t2, t3)
     assert t.type == TriangleType.INVALID
 
 def test_float_numbers():
-    t1 = 0.12
+    t1 = 0.012
     t2 = 0.004
     t3 = 0.156
     with pytest.raises(ValueError):
