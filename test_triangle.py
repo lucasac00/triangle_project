1e834e55a495f801b309dcbad9ca54fce57e8881
Lucas Cardoso <luc.fast2004@gmail.com>
2026-04-23 14:45:23 -0300

    feat: add more tests for edge cases

--- test_triangle.py
+++ test_triangle.py
@@ -40,3 +40,29 @@ def test_string():
 
     with pytest.raises(ValueError):
         Triangle(t1, t2, t3)
+        
+def test_boolean():
+    with pytest.raises(ValueError):
+        Triangle(True, False, True)
+        
+def test_sum_equals_third_side():
+    t = Triangle(2, 3, 5)
+    assert t.type == TriangleType.INVALID
+
+def test_isoceles_diff():
+    # side1 == side3 & side2 == side3
+    t1 = Triangle(10, 12, 10)
+    t2 = Triangle(12, 10, 10)
+    assert t1.type == TriangleType.ISOSCELES
+    assert t2.type == TriangleType.ISOSCELES
+
+def test_invalid_diff():
+    t1 = Triangle(5, 1, 1)
+    t2 = Triangle(1, 5, 1)
+    assert t1.type == TriangleType.INVALID
+    assert t2.type == TriangleType.INVALID
+    
+def test_single_zero():
+    # only one side = 0
+    t = Triangle(0, 3, 4)
+    assert t.type == TriangleType.INVALID
